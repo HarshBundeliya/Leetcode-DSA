e3e08d9197ee6bee6cce11142c450374fbbc1d3f
HarshBundeliya <147147262+HarshBundeliya@users.noreply.github.com>
2026-09-18 15:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (7.40%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,19 +2,10 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         # Time Complexity = O(n)
         # Space Complexity = O(n)
-        map = {}
+        seen = {}
         for i, num in enumerate(nums):
-            if num in map:
-                map[num].append(i)
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
             else:
-                map[num] = [i]
-        for i, num in enumerate(nums):
-            sec_num = target-num
-            if sec_num in map:
-                sec_num_index = map[sec_num]
-                if sec_num_index==[i]:
-                    continue
-                elif len(sec_num_index)==1:
-                    return [i,sec_num_index[0]]
-                else:
-                    return sec_num_index
+                seen[num] = i
