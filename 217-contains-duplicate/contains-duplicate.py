da6ad86c73b7dec19d5aea9a73475b6992671540
HarshBundeliya <147147262+HarshBundeliya@users.noreply.github.com>
2026-09-24 11:15:36 +0530

    Time: 14 ms (67.49%) | Memory: 32.4 MB (22.82%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -2,10 +2,9 @@ class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         # Time complexity = O(n)
         # Space complexity = O(n)
-        count_map = {}
+        seen = set()
         for num in nums:
-            if num in count_map:
+            if num in seen:
                 return True
-            else:
-                count_map[num] = 1
+            seen.add(num)
         return False
